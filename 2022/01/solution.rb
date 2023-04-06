cf7c363b610bdaf8265bbfce301dfd5f24621769
Joe Laurendi <joe@brightcellars.com>
2023-04-06 16:53:39 -0700

    Solved 2022 day 1 part 2

--- 2022/01/solution.rb
+++ 2022/01/solution.rb
@@ -1,5 +1,4 @@
-def part_one(input)
-    # lines = input.split("\n").map(&:to_i)
+def compute_elves(input)
     current_elf = 0
     elves = []
     input.each_line do |line|
@@ -11,11 +10,30 @@ def part_one(input)
             current_elf += line.to_i
         end
     end
+    if current_elf != 0
+        elves.push(current_elf)
+        current_elf = 0
+    end
+    return elves
+end
+
+def part_one(input)
+    elves = compute_elves(input)
     return elves.max
 end
 
 def part_two(input)
-    return "<incomplete>"
+    elves = compute_elves(input)
+    top_three = [0, 0, 0]
+    min_idx = 0
+    min_val = 0
+    elves.each do |elf|
+        if elf > min_val
+            top_three[min_idx] = elf
+            min_val, min_idx = top_three.each_with_index.min
+        end
+    end
+    return top_three.sum
 end
 
 input_file = File.join(File.dirname(__FILE__), "input.txt")
