ce13594cfa4a9b4a889a5992105b290d31ccb33a
Joe Laurendi <jlaurendi@gmail.com>
2024-12-04 07:16:45 -0800

    day 1 part 2

--- 2024/rust/src/bin/1.rs
+++ 2024/rust/src/bin/1.rs
@@ -1,11 +1,8 @@
 use std::fs;
 use std::path::Path;
+use std::collections::HashMap;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = Path::new("src/bin/input/01.txt");
-    let contents = fs::read_to_string(file_path).expect("Failed to read file");
-    // println!("{}", contents);
-
+fn parse_input(contents: &str) -> (Vec<i32>, Vec<i32>) {
     let number_pairs: Vec<(i32, i32)> = contents
         .lines()
         .filter(|line| !line.trim().is_empty())
@@ -19,14 +16,55 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .collect();
 
-    let (mut left, mut right): (Vec<i32>, Vec<i32>) = number_pairs.into_iter().unzip();
-    left.sort();
-    right.sort();
-    
+    return number_pairs.into_iter().unzip()
+}
+
+fn part1(left: &[i32], right: &[i32]) -> i32 {    
     let mut result = 0;
     for (a, b) in left.iter().zip(right.iter()) {
         result += (a - b).abs();
     }
-    println!("Part 1: {}", result);
+    return result;
+}
+
+fn part2(left: &[i32], right: &[i32]) -> i32 {
+    let mut left_counts: HashMap<i32, i32> = HashMap::new();
+    let mut right_counts: HashMap<i32, i32> = HashMap::new();
+    for left_num in left.iter() {
+        *left_counts.entry(*left_num).or_insert(0) += 1;
+    }
+    for right_num in right.iter() {
+        *right_counts.entry(*right_num).or_insert(0) += 1;
+    }
+
+    // println!("{:?}", left_counts);
+    // println!("{:?}", right_counts);
+
+    let mut result: i32 = 0;
+    for (num, left_count) in left_counts.iter() {
+        if right_counts.contains_key(num) {
+            let right_count = right_counts.get(num).unwrap();
+            result += num * left_count * right_count;
+        }
+    }
+    return result;
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = Path::new("src/bin/input/01.txt");
+    // let file_path = Path::new("src/bin/input/01-example.txt");
+
+    let contents = fs::read_to_string(file_path).expect("Failed to read file");
+
+    let (mut left, mut right) = parse_input(&contents);
+    left.sort();
+    right.sort();
+
+    let part1_result = part1(&left, &right);
+    println!("Part 1: {}", part1_result);
+
+    let part2_result = part2(&left, &right);
+    println!("Part 2: {}", part2_result);
+
     Ok(())
 }
